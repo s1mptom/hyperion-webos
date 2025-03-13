12a72004558c33935aaed29bbcd85c85907f787a
Pavel Turbin <p.turbin@berizaryad.ru>
2025-03-13 19:07:35 +0000

    Remove unnecessary blank lines in capture_init function

--- src/backends/libvtcapture.cpp
+++ src/backends/libvtcapture.cpp
@@ -61,7 +61,7 @@ int capture_init(cap_backend_config_t* config, void** state_p)
 
     // Sorry, no unlimited fps for you.
     self->props.frm = config->fps == 0 ? 60 : config->fps;
-    
+
     self->props.dump = DISPLAY_OUTPUT;
     if (HAS_QUIRK(config->quirks, QUIRK_ALTERNATIVE_DUMP_LOCATION))
         self->props.dump = SCALER_OUTPUT;
@@ -73,7 +73,7 @@ int capture_init(cap_backend_config_t* config, void** state_p)
         self->props.dump = OSD_OUTPUT;
     if (HAS_QUIRK(config->quirks, QUIRK_ALTERNATIVE_DUMP_LOCATION_5))
         self->props.dump = HISTOGRAM_OUTPUT;
-    
+
     self->props.loc.x = 0;
     self->props.loc.y = 0;
     self->props.reg.w = config->resolution_width;
